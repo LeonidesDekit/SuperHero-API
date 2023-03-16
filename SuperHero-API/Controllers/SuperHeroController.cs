e8309551da5ff03a410728c436c9a2e8d45d126c
LeonidesDekit <leodekit97@gmail.com>
2023-03-17 01:06:13 +0800

    Create  AddSuperHero and UpdateHero

--- SuperHero-API/Controllers/SuperHeroController.cs
+++ SuperHero-API/Controllers/SuperHeroController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHero_API.Data;
 
 namespace SuperHero_API.Controllers
 {
@@ -6,19 +8,44 @@ namespace SuperHero_API.Controllers
     [ApiController]
     public class SuperHeroController : Controller
     {
+        private readonly DataContext _context;
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> GetSuperHero()
         {
-            return new List<SuperHero>
-            {
-                new SuperHero
-                {
-                    Name = "Spider Man",
-                    FirstName = "Peter",
-                    LastName = "Parker",
-                    Place = "New York City",
-                }
-            };
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddSuperHero(SuperHero hero)
+        {
+            _context.Add(hero);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero hero)
+        {
+            var dbHero = _context.SuperHeroes.Find(hero.Id);
+            if(dbHero == null)
+                return BadRequest("Hero not found!");
+
+            dbHero.FirstName = hero.FirstName;
+            dbHero.Name = hero.Name;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
         }
     }
 }
