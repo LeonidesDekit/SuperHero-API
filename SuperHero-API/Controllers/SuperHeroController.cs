3bf5fcf29627da760e2e1b3bd3e28e17bda68eba
LeonidesDekit <leodekit97@gmail.com>
2023-03-16 23:08:35 +0800

    Create GetSuperHero Function

---
+++ SuperHero-API/Controllers/SuperHeroController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHero_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHero()
+        {
+            return new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City",
+                }
+            };
+        }
+    }
+}
